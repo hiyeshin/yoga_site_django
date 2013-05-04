c02ae3d27d1b60d16cb252b0779d4e6e6111f5ad
Hiye Shin <hiyeshin@gmail.com>
2013-05-04 14:39:54 -0400

    main page description added

--- yoga_site/urls.py
+++ yoga_site/urls.py
@@ -5,15 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    
     url(r'^$', 'yoga_front.views.home', name='home'),
-    # url(r'^main/', include('main.urls')),
     url(r'^main/', 'main.views.main', name = 'main'),
 	url(r'^ws_comm/', include('ws_comm.urls')),
 	url(r'^user_api/', include('user_api.urls')),
 	url(r'^displays/(?P<rpi_mac>.+)$', 'yoga_front.views.rpi_displays'),
-
-
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
